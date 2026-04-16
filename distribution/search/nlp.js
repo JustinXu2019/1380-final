ae56209080c1f4e60114514ba0e7cca81f092365
William Park <william_c_park@brown.edu>
2026-04-16 04:32:29 +0000

    nlp methods

---
+++ distribution/search/nlp.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const fs = require('fs');
+const {convert} = require('html-to-text');
+const natural = require('natural');
+
+const stopwordsPath = path.resolve(__dirname, '../../non-distribution/d/stopwords.txt');
+const stopwords = new Set(
+    fs.readFileSync(stopwordsPath, 'utf8').split(/\s+/).filter(Boolean),
+);
+const stemmer = natural.PorterStemmer;
+
+function tokenize(text) {
+  return text
+      .normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+      .replace(/[^a-zA-Z]/g, '\n').toLowerCase()
+      .split('\n').filter(Boolean)
+      .filter((w) => !stopwords.has(w))
+      .map((w) => stemmer.stem(w));
+}
+
+function ngrams(tokens) {
+  const out = [];
+  for (let i = 0; i < tokens.length; i++) {
+    out.push(tokens[i]);
+    if (i + 1 < tokens.length) out.push(`${tokens[i]} ${tokens[i + 1]}`);
+    if (i + 2 < tokens.length) out.push(`${tokens[i]} ${tokens[i + 1]} ${tokens[i + 2]}`);
+  }
+  return out;
+}
+
+function processText(text) {
+  const tokens = tokenize(text || '');
+  const grams = ngrams(tokens);
+  const counts = new Map();
+  for (const g of grams) {
+  if (counts.has(g)) {
+    counts.set(g, counts.get(g) + 1);
+  } else {
+    counts.set(g, 1);
+  }
+}
+  return Array.from(counts, ([ngram, count]) => ({ngram, count}));
+}
+
+function extractText(html) {
+  try {
+    return convert(html || '', {wordwrap: false});
+  } catch (err) {
+    return '';
+  }
+}
+
+function normalizeUrl(href, baseUrl) {
+  try {
+    return new URL(href, baseUrl).href.split('#')[0];
+  } catch {
+    return null;
+  }
+}
+
+function extractLinks(html, baseUrl) {
+  const regex = /<a[^>]*href=["']([^"']+)["']/gi;
+  const links = new Set();
+  
+  const matches = (html || '').matchAll(regex);
+  for (const match of matches) {
+    const url = normalizeUrl(match[1], baseUrl);
+    if (url) links.add(url);
+  }
+  
+  return Array.from(links);
+}
+
+function processQuery(q) {
+  return ngrams(tokenize(q || ''));
+}
+
+module.exports = {processText, processQuery, extractText, extractLinks, tokenize};
